1dfcc994d2cfc9b51f5119f641c3d6c3ea3f9942
ingu627 <ingu627@gmail.com>
2021-08-31 15:26:31 +0900

    doit

--- do_it_practice/Script_Part04.R
+++ do_it_practice/Script_Part04.R
@@ -55,19 +55,17 @@ mean(df_exam$science)
 df_exam_novar <- read_excel("./data/excel_exam_novar.xlsx")
 df_exam_novar
 
-df_exam_novar <- read_excel("excel_exam_novar.xlsx", col_names = F)
+df_exam_novar <- read_excel("./data/excel_exam_novar.xlsx", col_names = F)
 df_exam_novar
 
 # 엑셀 파일의 세 번째 시트에 있는 데이터 불러오기
-df_exam_sheet <- read_excel("excel_exam_sheet.xlsx", sheet = 3)
+df_exam_sheet <- read_excel("./data/excel_exam_sheet.xlsx", sheet = 3)
 df_exam_sheet
 
 
 ## -------------------------------------------------------------------- ##
-df_csv_exam <- read.csv("csv_exam.csv")
+df_csv_exam <- read.csv("./data/csv_exam.csv")
 df_csv_exam
-
-df_csv_exam <- read.csv("csv_exam.csv")
 
 
 ## -------------------------------------------------------------------- ##
@@ -75,17 +73,21 @@ df_midterm <- data.frame(english = c(90, 80, 60, 70),
                          math = c(50, 60, 100, 20),
                          class = c(1, 1, 2, 2))
 df_midterm
+str(df_midterm)
 
 write.csv(df_midterm, file = "df_midterm.csv")
-
+# 파일 생성
+# csv 는 범용적인 데이터파일
 
 ## -------------------------------------------------------------------- ##
 save(df_midterm, file = "df_midterm.rda")
+# rda = r전용 데이터
 
 rm(df_midterm)
+# rm = 메모리 상에서 제거 (remove)
 
 load("df_midterm.rda")
-
+# 파일 불러오기
 
 ## -------------------------------------------------------------------- ##
 # 1.변수 만들기, 데이터 프레임 만들기
